ca3c0f4655a2c2a3b75c4df1b585d19f12212880
Ben Slattery <ben.slattery@taitcommunications.com>
2023-10-06 15:36:15 +1300

    3 space formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,63 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
+void pollEvents(sf::RenderWindow& window)
+{
+   sf::Event event;
+   while (window.pollEvent(event))
+   {
+      switch (event.type)
+      {
+      case sf::Event::Closed:
+         window.close();
+         break;
+      case sf::Event::MouseMoved:
+         sf::Vector2i{event.mouseMove.x,event.mouseMove.y};
+         break;
+      case sf::Event::MouseButtonPressed:
+         if (event.mouseButton.button != sf::Mouse::Button::Left) break;
+         sf::Vector2i{event.mouseMove.x,event.mouseMove.y};
+         break;
+      case sf::Event::MouseButtonReleased:
+         if (event.mouseButton.button != sf::Mouse::Button::Left) break;
+         sf::Vector2i{event.mouseMove.x,event.mouseMove.y};
+         break;
+      default:
+         break;
+      }
+   }
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+   sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+   sf::CircleShape shape(100.f);
+   shape.setFillColor(sf::Color::Green);
 
 
-    sf::SoundBuffer buffer1;
-    sf::SoundBuffer buffer2;
+   sf::SoundBuffer buffer1;
+   sf::SoundBuffer buffer2;
 
-    buffer1.loadFromFile("samples/17.wav");
-    buffer2.loadFromFile("samples/16.wav");
+   buffer1.loadFromFile("samples/17.wav");
+   buffer2.loadFromFile("samples/16.wav");
 
-    sf::Sound sound1;
-    sf::Sound sound2;
+   sf::Sound sound1;
+   sf::Sound sound2;
 
-    sound1.setBuffer(buffer1);
-    sound2.setBuffer(buffer2);
+   sound1.setBuffer(buffer1);
+   sound2.setBuffer(buffer2);
 
-    sound1.play();
-    sound2.play();
+   sound1.play();
+   sound2.play();
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+   while (window.isOpen())
+   {
+      pollEvents(window);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+      window.clear();
+      window.draw(shape);
+      window.display();
+   }
 
-    return 0;
+   return 0;
 }
